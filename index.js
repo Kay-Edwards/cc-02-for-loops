6f93db4180cd672b9fd8c94b9a6dc51d7df99d48
honeychuckles <kaylaedwards575@gmail.com>
2021-09-29 20:40:35 +0000

    Finished code challenge

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
 //1
 function countToTen(){
-  
+    for (let i=1; i<=10; i++){
+        console.log(i)
+    }
 }
 
 //2
-function countFromOne(){
-  
+function countFromOne(number){
+for (let k=1; k<= number; k++){
+    console.log(k)
+}    
 }
 
 //3
-function countEveryEven(){
-  
+function countEveryEven(n){
+    for(let i =0; i<=n; i++){
+    if (i%2===0){
+        console.log(i)
+    }
+    }
 }
 
 //4
-function countEveryOdd(){
-  
+function countEveryOdd(y){
+  for (let i=0; i<=y; i++){
+      if(i%2 >0){
+          console.log(i)
+  }
+  }
 }
 
 //5 
-function countEvens(){
-  
+let evennum=0;
+function countEvens(array)
+{
+    for (let i=0;i< array.length; i++){
+        if (array[i]%2===0){
+        evennum++
+        }
+    }
+  return evennum
 }
 
+//countToTen()
+//countFromOne(90)
+//countEveryEven(10)
+//countEveryOdd(10)
+//countEven([1,3,7,17,19])
+console.log(countEvens([1,3,7,16,19]))
